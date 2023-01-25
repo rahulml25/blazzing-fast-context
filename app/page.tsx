fd5de7f17bf0a2913570a332f73719dcaaf4e6b3
rahulml2505 <rahulml2505@gmail.com>
2023-01-25 14:25:45 +0530

    made this generic alos!!!!! (Blazing Fast Context)

--- app/page.tsx
+++ app/page.tsx
@@ -1,85 +1,23 @@
 "use client";
 
 import "@/styles/tailwind.css";
-import React, {
-  createContext,
-  useCallback,
-  useContext,
-  useRef,
-  useSyncExternalStore,
-} from "react";
 import ContentContainer from "@/components/ContentContainer";
+import createFastContext from "@/lib/createFastContext";
 
-type Store = {
-  first: string;
-  last: string;
-};
+const { Provider, useStore } = createFastContext({
+  first: "",
+  last: "",
+});
 
-export function useStoreData(): {
-  get(): Store;
-  set(value: Partial<Store>): void;
-  subscribe(callback: () => void): () => void;
-} {
-  const store = useRef({
-    first: "",
-    last: "",
-  });
-
-  const get = useCallback(() => store.current, []);
-
-  const subscribers = useRef(new Set<() => void>());
-
-  const set = useCallback((value: Partial<Store>) => {
-    store.current = { ...store.current, ...value };
-    subscribers.current.forEach((callback) => callback());
-  }, []);
-
-  const subscribe = useCallback((callback: () => void) => {
-    subscribers.current.add(callback);
-    return () => subscribers.current.delete(callback);
-  }, []);
-
-  return {
-    get,
-    set,
-    subscribe,
-  };
-}
-
-export function useStore<SelectorOutput>(
-  selector: (store: Store) => SelectorOutput
-): [SelectorOutput, (value: Partial<Store>) => void] {
-  const store = useContext(StoreContext);
-  if (!store) {
-    throw new Error("Store not found");
-  }
-
-  const state = useSyncExternalStore(store.subscribe, () =>
-    selector(store.get())
-  );
-
-  return [state, store.set];
-}
-
-type useStoreDataReturnType = ReturnType<typeof useStoreData>;
-
-export const StoreContext = createContext<useStoreDataReturnType>(
-  {} as unknown as useStoreDataReturnType
-);
-
-const StoreContextProvider = ({ children }: { children: React.ReactNode }) => (
-  <StoreContext.Provider value={useStoreData()}>
-    {children}
-  </StoreContext.Provider>
-);
+export { useStore };
 
 export default function HomePage() {
   return (
-    <StoreContextProvider>
+    <Provider>
       <div className="max-w-xl mx-auto py-20 px-9 border mt-12">
         <h1 className="font-bold text-4xl mb-7">App</h1>
         <ContentContainer />
       </div>
-    </StoreContextProvider>
+    </Provider>
   );
 }
--- lib/createFastContext.tsx
+++ lib/createFastContext.tsx
@@ -1,8 +1,73 @@
-import React from 'react'
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useRef,
+  useSyncExternalStore,
+} from "react";
 
-export default function createFastContext() {
-  return (
-    <div></div>
-  )
+export default function createFastContext<Store>(initalState: Store) {
+  function useStoreData(): {
+    get(): Store;
+    set(value: Partial<Store>): void;
+    subscribe(callback: () => void): () => void;
+  } {
+    const store = useRef(initalState);
+
+    const get = useCallback(() => store.current, []);
+
+    const subscribers = useRef(new Set<() => void>());
+
+    const set = useCallback((value: Partial<Store>) => {
+      store.current = { ...store.current, ...value };
+      subscribers.current.forEach((callback) => callback());
+    }, []);
+
+    const subscribe = useCallback((callback: () => void) => {
+      subscribers.current.add(callback);
+      return () => subscribers.current.delete(callback);
+    }, []);
+
+    return {
+      get,
+      set,
+      subscribe,
+    };
+  }
+
+  type useStoreDataReturnType = ReturnType<typeof useStoreData>;
+
+  const StoreContext = createContext<useStoreDataReturnType>(
+    {} as unknown as useStoreDataReturnType
+  );
+
+  function useStore<SelectorOutput>(
+    selector: (store: Store) => SelectorOutput
+  ): [SelectorOutput, (value: Partial<Store>) => void] {
+    const store = useContext(StoreContext);
+    if (!store) {
+      throw new Error("Store not found");
+    }
+
+    const state = useSyncExternalStore(store.subscribe, () =>
+      selector(store.get())
+    );
+
+    return [state, store.set];
+  }
+
+  const Provider = ({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) => (
+    <StoreContext.Provider value={useStoreData()}>
+      {children}
+    </StoreContext.Provider>
+  );
+
+  return {
+    Provider,
+    useStore,
+  };
 }
-
